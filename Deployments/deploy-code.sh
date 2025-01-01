721e08d8a7aa7b929d60e893b0c9b1a66ae377ad
Tom Bulatewicz <tom@nosleep.net>
2025-01-01 11:29:23 -0800

    Upgrade to .NET 9

--- Deployments/deploy-code.sh
+++ Deployments/deploy-code.sh
@@ -11,7 +11,7 @@ echo "Deploying to $function_app..."
 cd ../Nabster.Chat.Functions
 dotnet clean --configuration Release /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary
 dotnet publish --configuration Release /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary
-cd bin/Release/net8.0/publish/
+cd bin/Release/net9.0/publish/
 zip functions.zip . -r
 az functionapp deployment source config-zip -g $resource_group -n $function_app --src functions.zip 
 rm functions.zip
